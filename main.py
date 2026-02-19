5827684fb4ea737c5feffc1d5a68cd56afd9428a
Julián González Calderón <gonzalezcalderonjulian@gmail.com>
2026-02-19 01:26:08 +0000

    Abstract some function and strip whitespace from fields

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
-import sys
-from playwright.sync_api import sync_playwright
 import json
+import sys
+
+from playwright.sync_api import sync_playwright, Page
+
+
+def locate_text(page: Page, selector: str) -> str | None:
+    text = page.locator(selector).text_content()
+    if text:
+        text.strip()
+    return text
+
+
+def locate_url(page: Page, selector: str) -> str | None:
+    return page.locator(selector).get_attribute("href")
 
 
 def main(url: str):
@@ -14,36 +26,35 @@ def main(url: str):
         org_cards = page.locator("app-orgs-card")
 
         while True:
-            page.wait_for_load_state("networkidle")
+            org_cards.first.wait_for()
             for org_card in org_cards.all():
                 org_anchor = org_card.locator("a")
                 with context.expect_page() as org_page_event:
                     org_anchor.click()
-
                 org_page = org_page_event.value
-                org_page.wait_for_load_state("networkidle")
-                org = {
-                    "title": org_page.locator(
-                        "app-org-page-title .title"
-                    ).text_content(),
-                    "subtitle": org_page.locator("app-org-info .hd").text_content(),
-                    "technologies": org_page.locator(
-                        "app-org-info .tech__content"
-                    ).text_content(),
-                    "topics": org_page.locator(
-                        "app-org-info .topics__content"
-                    ).text_content(),
-                    "link": org_page.locator(
-                        "app-org-info .link__wrapper a"
-                    ).get_attribute("href"),
-                    "ideas": org_page.locator(".button-wrapper a").get_attribute(
-                        "href"
-                    ),
-                    "summary": org_page.locator("app-org-info .bd").text_content(),
-                }
-                org_page.close()
 
-                print(json.dumps(org, indent=4))
+                title = locate_text(org_page, "app-org-page-title .title")
+                subtitle = locate_text(org_page, "app-org-info .hd")
+                tech = locate_text(org_page, "app-org-info .tech__content")
+                topics = locate_text(org_page, "app-org-info .topics__content")
+                link = locate_url(org_page, "app-org-info .link__wrapper a")
+                ideas = locate_url(org_page, ".button-wrapper a")
+                summary = locate_text(org_page, "app-org-info .bd")
+
+                org = {
+                    "title": title,
+                    "subtitle": subtitle,
+                    "technologies": tech,
+                    "topics": topics,
+                    "link": link,
+                    "ideas": ideas,
+                    "summary": summary,
+                }
+                print(json.dumps(org, ensure_ascii=True, indent=4))
+
+                org_page.close()
+                break
+            break
 
             if next_button.is_disabled():
                 break
